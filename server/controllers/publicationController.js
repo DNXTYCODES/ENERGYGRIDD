e060d809a184ae1ac514a1094846e8d2eca57d9d
MatthewCodez <mayodele113@gmail.com>
2025-01-27 19:30:41 +0100

    publication exports

--- server/controllers/publicationController.js
+++ server/controllers/publicationController.js
@@ -1,11 +1,11 @@
 import { prisma } from "../config/prismaConfig.js";
 
-// Create a publication with associated authors
+// Create a publication
 export const createPublication = async (req, res, next) => {
   try {
     const { image, title, content, category, writerIds } = req.body;
 
-    // Validate that all provided writerIds exist
+    // Validate writerIds
     const authors = await prisma.author.findMany({ where: { writerId: { in: writerIds } } });
     const foundWriterIds = authors.map((author) => author.writerId);
 
@@ -33,7 +33,7 @@ export const createPublication = async (req, res, next) => {
   }
 };
 
-// Get all publications with their authors
+// Get all publications
 export const getPublications = async (req, res, next) => {
   try {
     const publications = await prisma.publication.findMany({
@@ -44,3 +44,113 @@ export const getPublications = async (req, res, next) => {
     next(err);
   }
 };
+
+// Update a publication
+export const updatePublication = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { image, title, content, category, writerIds } = req.body;
+
+    // Validate writerIds
+    const authors = await prisma.author.findMany({ where: { writerId: { in: writerIds } } });
+    const foundWriterIds = authors.map((author) => author.writerId);
+
+    const invalidWriterIds = writerIds.filter((id) => !foundWriterIds.includes(id));
+    if (invalidWriterIds.length > 0) {
+      return res.status(400).json({
+        error: `The following writerIds do not exist: ${invalidWriterIds.join(", ")}`,
+      });
+    }
+
+    // Update publication and its authors
+    const updatedPublication = await prisma.publication.update({
+      where: { id },
+      data: {
+        image,
+        title,
+        content,
+        category,
+        authors: {
+          deleteMany: {}, // Clear existing authors
+          create: authors.map((author) => ({ authorId: author.id })), // Add new authors
+        },
+      },
+    });
+
+    res.status(200).json(updatedPublication);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete a publication
+export const deletePublication = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    // Delete publication
+    await prisma.publication.delete({ where: { id } });
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { prisma } from "../config/prismaConfig.js";
+
+// // Create a publication with associated authors
+// export const createPublication = async (req, res, next) => {
+//   try {
+//     const { image, title, content, category, writerIds } = req.body;
+
+//     // Validate that all provided writerIds exist
+//     const authors = await prisma.author.findMany({ where: { writerId: { in: writerIds } } });
+//     const foundWriterIds = authors.map((author) => author.writerId);
+
+//     const invalidWriterIds = writerIds.filter((id) => !foundWriterIds.includes(id));
+//     if (invalidWriterIds.length > 0) {
+//       return res.status(400).json({
+//         error: `The following writerIds do not exist: ${invalidWriterIds.join(", ")}`,
+//       });
+//     }
+
+//     const publication = await prisma.publication.create({
+//       data: {
+//         image,
+//         title,
+//         content,
+//         category,
+//         authors: {
+//           create: authors.map((author) => ({ authorId: author.id })),
+//         },
+//       },
+//     });
+//     res.status(201).json(publication);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
+
+// // Get all publications with their authors
+// export const getPublications = async (req, res, next) => {
+//   try {
+//     const publications = await prisma.publication.findMany({
+//       include: { authors: { include: { author: true } } },
+//     });
+//     res.status(200).json(publications);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
