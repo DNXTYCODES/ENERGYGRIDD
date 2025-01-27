7577c2f039b9ef733c0e85572d05a1e68e0030ae
MatthewCodez <mayodele113@gmail.com>
2025-01-27 09:24:25 +0100

    route update for crud operations

--- server/routes/authorRoutes.js
+++ server/routes/authorRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createAuthor, getAuthors } from "../controllers/authorController.js";
+import { createAuthor, getAuthors, updateAuthor, deleteAuthor } from "../controllers/authorController.js";
 
 const router = express.Router();
 
 router.post("/", createAuthor);
 router.get("/", getAuthors);
+router.put("/:id", updateAuthor);
+router.delete("/:id", deleteAuthor);
 
 export default router;
 
--- server/routes/publicationRoutes.js
+++ server/routes/publicationRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createPublication, getPublications } from "../controllers/publicationController.js";
+import { createPublication, getPublications, updatePublication, deletePublication } from "../controllers/publicationController.js";
 
 const router = express.Router();
 
 router.post("/", createPublication);
 router.get("/", getPublications);
+router.get("/:id", updatePublication);
+router.get("/:id", deletePublication);
 
 export default router;
