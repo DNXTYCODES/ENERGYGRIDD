624ab1eded53357f8cb7318bd10d5d0ea7ef78c1
MatthewCodez <mayodele113@gmail.com>
2025-01-27 06:40:33 +0100

    prisma model field update

--- server/controllers/authorController.js
+++ server/controllers/authorController.js
@@ -3,7 +3,7 @@ import { prisma } from "../config/prismaConfig.js";
 // Create an author with a unique writerId
 export const createAuthor = async (req, res, next) => {
   try {
-    const { writerId, name, role, about, experience } = req.body;
+    const { writerId, image, name, role, about, email, linkedIn, phone, experience } = req.body;
 
     // Validate uniqueness of writerId
     const existingAuthor = await prisma.author.findUnique({ where: { writerId } });
@@ -12,7 +12,7 @@ export const createAuthor = async (req, res, next) => {
     }
 
     const author = await prisma.author.create({
-      data: { writerId, name, role, about, experience },
+      data: { writerId, image, name, role, about, email, linkedIn, phone,  experience },
     });
     res.status(201).json(author);
   } catch (err) {
@@ -31,43 +31,3 @@ export const getAuthors = async (req, res, next) => {
     next(err);
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { prisma } from "../config/prismaConfig.js";
-
-// // Create an author
-// export const createAuthor = async (req, res, next) => {
-//   try {
-//     const { name, role, about, experience } = req.body;
-//     const author = await prisma.author.create({
-//       data: { name, role, about, experience },
-//     });
-//     res.status(201).json(author);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-// // Get all authors with their publications
-// export const getAuthors = async (req, res, next) => {
-//   try {
-//     const authors = await prisma.author.findMany({
-//       include: { publications: { include: { publication: true } } },
-//     });
-//     res.status(200).json(authors);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
--- server/controllers/publicationController.js
+++ server/controllers/publicationController.js
@@ -3,7 +3,7 @@ import { prisma } from "../config/prismaConfig.js";
 // Create a publication with associated authors
 export const createPublication = async (req, res, next) => {
   try {
-    const { title, content, writerIds } = req.body;
+    const { title, content, category, writerIds } = req.body;
 
     // Validate that all provided writerIds exist
     const authors = await prisma.author.findMany({ where: { writerId: { in: writerIds } } });
@@ -20,6 +20,7 @@ export const createPublication = async (req, res, next) => {
       data: {
         title,
         content,
+        category,
         authors: {
           create: authors.map((author) => ({ authorId: author.id })),
         },
@@ -42,45 +43,3 @@ export const getPublications = async (req, res, next) => {
     next(err);
   }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-// // Create a publication with associated authors
-// export const createPublication = async (req, res, next) => {
-//   try {
-//     const { title, content, authorIds } = req.body;
-//     const publication = await prisma.publication.create({
-//       data: {
-//         title,
-//         content,
-//         authors: {
-//           create: authorIds.map((authorId) => ({ authorId })),
-//         },
-//       },
-//     });
-//     res.status(201).json(publication);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-// // Get all publications with their authors
-// export const getPublications = async (req, res, next) => {
-//   try {
-//     const publications = await prisma.publication.findMany({
-//       include: { authors: { include: { author: true } } },
-//     });
-//     res.status(200).json(publications);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
